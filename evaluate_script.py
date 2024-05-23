38f305a68537430bb11d7366deec16ca4884c062
Hyunsoo Kim <khs0415p@naver.com>
2024-05-23 16:58:41 +0900

    update gpu device

--- evaluate_script.py
+++ evaluate_script.py
@@ -1,19 +1,20 @@
 import torch
 import pandas as pd
 
-from transformers import AutoTokenizer, BertModel
+from transformers import AutoTokenizer, AutoModel
 
 
 threshold = 0.779
-tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-v1.1")
-model = BertModel.from_pretrained("dmis-lab/biobert-v1.1")
-device = torch.device("cpu")
+tokenizer = AutoTokenizer.from_pretrained('tokenizer/tokenizer-32k-total')
+model = AutoModel.from_pretrained('1802631-steps/')
+device = torch.device("cuda")
 model.to(device)
 model.eval()
 
-def cos_sim(row, eps=1e-08):
+def cos_sim(row, eps=1e-08, mode='mean'):
     a, b = row
-    a, b = a['last_hidden_state'].mean(dim=1).squeeze(), b['last_hidden_state'].mean(dim=1).squeeze()
+    a, b = a['last_hidden_state'].detach().cpu().mean(dim=1).squeeze(), b['last_hidden_state'].detach().cpu().mean(dim=1).squeeze()
+    
     numerator = a @ b
     a_l2, b_l2 = a @ a, b @ b
     denominator = torch.sqrt(torch.mul(a_l2, b_l2)) + torch.tensor(eps)
@@ -22,16 +23,17 @@ def cos_sim(row, eps=1e-08):
 
 def get_logits(row):
     a = {k: v.to(device) for k, v in row[0].items()}
-    b = {k: v.to(device) for k, v in row[0].items()}
+    b = {k: v.to(device) for k, v in row[1].items()}
     output_a = model(**a)
     output_b = model(**b)
+    output_a = {k:v.detach().cpu() for k, v in output_a.items()}
+    output_b = {k:v.detach().cpu() for k, v in output_b.items()}
 
     return output_a, output_b
 
 
 def get_metric(preds, labels, threshold):
-    preds = preds >= threshold
-    preds = preds.astype('int8')
+    preds = (preds >= threshold)
 
     labels = labels.values
     preds = preds.values
